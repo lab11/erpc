c9f63afb47606e4be3298e88b9b41d0e4950aadc
Josh Adkins <adkinsjd@umich.edu>
2016-12-02 17:32:57 -0800

    added an example crypto program

---
+++ erpc_python/test_crypto_server.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+
+import sys
+import math
+import random
+import erpc
+#from Crypto.Signature import PKCS1_v1_5
+#from Crypto.Hash import SHA
+#from Crypto.PublicKey import RSA
+
+import test_crypt
+
+def server():
+    class CryptoServiceHandler(test_crypt.interface.ICrypto):
+        def sign(self, data):
+            #key = RSA.generate(2048)
+            #h = SHA.new(data)
+            #signer = PKCS1_v1_5.new(key)
+            #signature = signer,sign(h)
+            for i, val in data:
+                data[i] = val+1
+
+            return ret(data)
+
+    transport = erpc.transport.I2CTransport(6,0x19)
+    server = erpc.simple_server.SimpleServer(transport, erpc.basic_codec.BasicCodec)
+    cryptoHandler = CryptoServiceHandler()
+    cryptoService = erpc_test.server.CryptoService(cryptoHandler)
+    server.add_service(cryptoService)
+    server.run()
+
+if __name__ == "__main__":
+    server()
+
